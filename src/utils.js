c9b2801bfbcdd92b6484f5da3d132f63072b17e6
Joanne Kagio <kagiojoanne@gmail.com>
2025-10-22 22:04:30 +0300

    Initial commit

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
 // Your code here
+export function currentAgeForBirthYear(birthYear) {
+  // Get the current year dynamically
+  const currentYear = new Date().getFullYear();
+
+  // Return the difference between current year and birth year
+  return currentYear - birthYear;
+}
